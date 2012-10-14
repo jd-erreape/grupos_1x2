d79d41dda3066e5cda7627e0fba09fda3352b22f
DI2uNk <jd.drunk@gmail.com>
2012-10-14 19:48:11 +0200

    Solución bug desarrollo

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -4,13 +4,14 @@ class WelcomeController < ApplicationController
   before_filter :load_last_round, :only => [:index]
 
   def reload_matches
-    render 'index.html.erb'
+    render :nothing => true
   end
 
   protected
 
   def load_new_bet_data
     @bet_data = BetBuilder.new(true)
+    @bet_data.load_matches
   end
 
   def load_last_round
